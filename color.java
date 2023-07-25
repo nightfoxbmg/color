5ec037223edbfb141a89475db792e3895234348a
nightfoxbmg <mishabalabanpw@gmail.com>
2023-07-25 18:31:34 +0300

    init commit

---
+++ color.java
@@ -0,0 +1,16 @@
+package otus.hw1.color;
+
+public class color {
+    public static void main(String[] args) {
+        int colour = 5;
+        if (colour >= 0) {
+            System.out.println("Red");
+        }
+        if (colour <= 10) {
+            System.out.println("Yellow");
+        } else {
+            System.out.println("Green");
+        }
+        }
+    }
+
